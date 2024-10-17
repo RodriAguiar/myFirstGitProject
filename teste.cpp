a5150e364871a07684bf6c4f5811edaf00488443
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-10-17 10:20:03 +0000

    dawdawd

--- teste.cpp
+++ teste.cpp
@@ -1,8 +1,61 @@
-#include <iostream>
-using namespace std;
+#include <iostream>//usar sempre
+using namespace std;//usar sempre proteje e ajuda o código casulo ou quarto
 
-int main() {
+int main() {//main é a função principal tudo dentro das {} é um bloco de código que pertence á função main
     cout << "hello World!" << endl;
-    return 0;
+
+
+
+
+ 
+//adicção de números
+    cout << 3 + 3 << endl;
+
+    //subtração de números
+    cout << 3 - 3 << endl;
+
+    //Multiplicação de números
+    cout << 3 * 3 << endl;
+
+    //Divisão de números
+    cout << 3 / 3 << endl;
+
+    //Resto da divisão
+    cout << 5 % 3 << endl;
+
+
+//Variáveis sempre minusculas camel case, numca começar por um número
+int quntLivros = 5;//int=inteiro
+int x, y, z;
+x = 25;
+cout << quntLivros * 2 << endl;
+cout << quntLivros + 5 << endl;
+cout << quntLivros - 2 << endl;
+
+
+bool isTrue = false;
+bool isHIgh;
+isHIgh = true;
+
+float altura = 1.75;
+float peso;
+peso =34,6;
+
+double altura2 = 1.75;
+double peso2;
+peso2 = 34.6;
+
+char letra = 'a';
+char letra2;
+letra2 = 'b';
+
+string nome = "Lucas";// string é um conjunto de chars 
+string sobrenome;
+sobrenome = "Santos";
+
+    cout << " Nome " << nome << " " << sobrenome << endl;
+    
+return 0;
+
 
 }
