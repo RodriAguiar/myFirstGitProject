8c749a19c870eb5c1961b43602b92b5a9a8777f7
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-10-21 15:31:34 +0000

    wadwadw

--- teste.cpp
+++ teste.cpp
@@ -56,16 +56,55 @@ sobrenome = "Santos";
     cout << " Nome " << nome << " " << sobrenome << endl;
 
 //2 * pi * r
-const double pi = 3.1416;//const=constates é um valor que é não pode variar e nem ser alterado no codigo
+const double PI = 3.1416;//const=constates é um valor que é não pode variar e nem ser alterado no codigo e são sempre maiusculas
 const int y = 2;
 
 float valor;
 
 cout << "idique o valor do raio";
 cin >> valor;
-cout << y * pi * 5 << endl;
+cout << y * PI * 5 << endl;
 
+//operadores lógicos
+// ==    Igual
+// !=    Diferente
+// <     Maior   
+// >     Menor
+// >=    Maior ou igual 
+// <=    Menor ou igual
+// &&    e
+// ||    OU
+
+
+
+int abc; 
+if( abc == 'a'){
+
+cout << "abc é igual a a" << endl;
+    }else{
+        cout << "abc é diferente de a" << endl;
+
+    }
+
+
+int ( x == 1);
+int ( y == 2);
+
+if (y == 2 ) {
+    cout << "v igual a dois" << endl;
+}else if(y == 3){
+    cout << "y é igual a três" << endl;
+}else if(y  == 4){
+    cout << "y é igual a quatro" << endl;
+
+}else{
+    cout << "nenhum dos valores é igual a 2, 3 ou 4" << endl;
+
+}
+
+
+//if comparar valores
+//else if vai ser chamada se a anterior for chamada
+//switch é para selecionar um dos blocos de comando
 return 0;
-
-
 }
