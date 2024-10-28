817bca0716cf50003bf7c9b2f5805124133cdb23
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-10-28 22:03:32 +0000

    exercicio_cicloFor.cpp

--- exercicio_cicloFor.cpp
+++ exercicio_cicloFor.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    // Criação de um array para armazenar os números pares
+    int array[10]; // Existem 10 números pares de 1 a 20
+    int index = 0;
 
-int array[20];
+    // Preenchendo o array com números pares de 1 a 20
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 == 0) { // Verifica se o número é par
+            array[index] = i;
+            index++;
+        }
+    }
 
-for(int i=0; i<20; i++){
+    // Exibindo os números pares armazenados no array
+    cout << "Números pares de 1 a 20:" << endl;
+    for (int i = 0; i < index; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 
-}
-
-return 0;
+    return 0;
 }
