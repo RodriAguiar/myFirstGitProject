831f6680ff14a28c3aa780065767b12010a79ad7
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-10-30 09:09:39 -0100

    Update exercicio_cicloFor.cpp

--- exercicio_cicloFor.cpp
+++ exercicio_cicloFor.cpp
@@ -4,9 +4,9 @@
 
 int main () {
 
-int numerospares [21] = { 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 };
-for (int x=0 ; x <= 20; x+=2){
-   
+int numerospares [20] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 };
+for (int x=0 ; x <= 20; x++){
+   if( numerospares [x] %2 == 0 )
     cout << numerospares [x] << endl;
 
 }
