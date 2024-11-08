b2e685094e362f2aafb40292057d2015ee06f6d3
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-11-08 18:30:17 +0000

    Create TPC_raio.cpp

---
+++ TPC_raio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+const float PI=3.1416;
+float raio;
+
+int main() {
+    cout << "idique o valor do raio" << endl;
+cin >> raio;
+
+cout << "O perimetro da circunferência  é: "<< endl;
+cout << 2*PI*raio << endl;
+
+cout << "A área da circunferência é: " << endl;
+cout << PI*(raio*raio)<< endl;
+
+
+    return 0;
+}
