8999fcac0157c66ba1a9683c582f6c60b053e742
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-11-08 18:27:15 +0000

    Create tpc_metrosmilimetros.cpp

---
+++ tpc_metrosmilimetros.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+float pedido;
+int main()
+{
+    cout << "digite uma medida em metros que queria em milimetros";
+ cin >> pedido;
+ cout << "a medição em milimetros é: "<< pedido *1000 << endl;
+ return 0;
+} 
