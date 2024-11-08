8a0837807be31caa610afddc9d1ef2d7168df328
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-11-08 16:28:30 +0000

    Atualizar o tpc.cpp

--- tpc.cpp
+++ tpc.cpp
@@ -1,1 +1,25 @@
-duddhdhdhd
+#include <iostream>
+using namespace std;
+float horas;
+float salario;
+float descontoIRS
+float IRS = 0.125;
+
+int main () {
+   
+cout << "Quantas horas trabalhaste?" << endl;
+cin >> horas;
+
+cout << "Quanto ganhas por hora?" << endl;
+cin >> salario;
+
+cout << "Vais receber:" << endl;
+cout << horas * salario << "Euros" << endl;
+
+cout << "Com o desconto do IRS vais receber:" << endl;
+cout << descontoIRS = horas*salario << endl;
+cout << descontoIRS * IRS << " Euros " << endl;
+
+   
+    return 0;
+}
