e50301cd58d23bd862de70dc2ee0cffb7d6fc279
RodriAguiar <rodrigoaguiar085@gmail.com>
2024-12-03 20:44:13 +0000

    jogo_adivinhaletra.cpp

--- jogo_adivinhaletra.cpp
+++ jogo_adivinhaletra.cpp
@@ -5,20 +5,19 @@
 using namespace std;
 
 int main() {
-    // Inicializa o gerador de números aleatórios
     srand(time(0));
     
-    // Array completo do alfabeto em minúsculas
+   
     char alfabeto[] = {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
         'u', 'v', 'w', 'x', 'y', 'z'
     };
 
-    // Escolhe uma letra aleatória
+    
     char letraEscolhida = alfabeto[rand() % 26];
     
-    // Variáveis do jogo
+   
     char adivinhação;
     int tentativas = 6;
     bool acertou = false;
@@ -27,12 +26,12 @@ int main() {
     cout << "Tenta adivinhar a letra escolhida pelo computador." << endl;
     cout << "Tens " << tentativas << " tentativas." << endl;
 
-    // Loop do jogo
+ 
     while (tentativas > 0) {
         cout << "\nDigite uma letra (a-z): ";
         cin >> adivinhação;
 
-        // Verifica se a entrada é válida
+       
         if (adivinhação < 'a' || adivinhação > 'z') {
             cout << "Letra inválida! Por favor, coloque uma letra minúscula (a-z)." << endl;
             continue;
@@ -51,7 +50,7 @@ int main() {
         cout << "Tens " << tentativas << " tentativa(s) restante(s)." << endl;
     }
 
-    // Resultado do jogo
+ 
     if (acertou) {
         cout << "\nParabéns! Acertaste a letra escolhida: " << letraEscolhida << "!" << endl;
     } else {
