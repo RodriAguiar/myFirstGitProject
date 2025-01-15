3e0fe800fa745819df0659bfa3402389fc4fc843
RodriAguiar <rodrigoaguiar085@gmail.com>
2025-01-15 14:38:42 -0100

    Update Avaliação_2.cpp

--- Avaliação_2.cpp
+++ Avaliação_2.cpp
@@ -13,9 +13,43 @@ cout << "Olá bem-vindo à simulação do lançamento da moeda" << endl;
 cout << "Esta é a sua moeda" << endl;
 cout << "MOEDA: " << endl;
     
-    cout << "Pressione [ENTER] para girar a moeda." << endl;
-    int x;
-    cin.ignore();
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+bool cara = false;
+bool coroa = false;
+int continuar;
+
+int main(){
+ srand(time(0));
+cout << "Olá bem-vindo à simulação do lançamento da moeda" << endl;
+//Imprimir a moeda no terminal
+//Pedir ao jogador para girar a moeda
+//Imprimir a moeda de novo mas com a face da cara ou da coroa
+cout << "Esta é a sua moeda" << endl;
+cout << "MOEDA: " << endl;
+cout << "Se quiser girar a moeda pressione 8 " << endl;    
+cin >> continuar;
+
+switch (expression)
+{
+case /* constant-expression */:
+    /* code */
+    break;
+
+default:
+    break;
+}
+if (continuar != 8){
+    cout << "Insira o número 8 para girar a moeda" << endl;
+}
+
+
+
+return 0;
+}
 
 
 
