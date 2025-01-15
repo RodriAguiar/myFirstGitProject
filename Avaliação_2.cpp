b07c08be0e7986193bdd2b3d298c7b3dfd39bbfd
RodriAguiar <rodrigoaguiar085@gmail.com>
2025-01-15 10:42:47 -0100

    Avaliação_2.cpp

---
+++ Avaliação_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+int main(){
+ srand(time(0));
+cout << "Olá bem-vindo à simulação do lançamento da moeda" << endl;
+//Imprimir a moeda no terminal
+//Pedir ao jogador para girar a moeda
+//Imprimir a moeda de novo mas com a face da cara ou da coroa
+cout << "Esta é a sua moeda" << endl;
+cout << "MOEDA: " << endl;
+    
+    cout << "Pressione [ENTER] para girar a moeda." << endl;
+    int x;
+    cin.ignore();
+
+
+
+return 0;
+}
