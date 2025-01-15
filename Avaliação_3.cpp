042884c29e7be5fa8e9b3564d003f498f37a20f6
RodriAguiar <rodrigoaguiar085@gmail.com>
2025-01-15 10:43:39 -0100

    Avaliação_3.cpp

--- Avaliação_3.cpp
+++ Avaliação_3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string palavra;
+
+
+int main(){
+
+cout << "Olá és o jogador 1. Seleciona uma palavra para o jogador 2 adivinhar." << endl;
+
+
+    return 0;
+}
+
 
